d3f950ed82e00b289bc91215dba2eec916ec83d7
Tam <thanhtam0933019816@gmail.com>
2025-09-16 09:53:24 +0700

    test CI

--- ProductService/src/main/java/intern/lp/config/RabbitConfig.java
+++ ProductService/src/main/java/intern/lp/config/RabbitConfig.java
@@ -7,20 +7,21 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//Test
 @Configuration
 @EnableRabbit
 public class RabbitConfig {
 
-        @Bean
-        public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-            return new Jackson2JsonMessageConverter();
-        }
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
-        @Bean
-        public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-            RabbitTemplate template = new RabbitTemplate(connectionFactory);
-            template.setMessageConverter(jackson2JsonMessageConverter());
-            return template;
-        }
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jackson2JsonMessageConverter());
+        return template;
+    }
 
 }
