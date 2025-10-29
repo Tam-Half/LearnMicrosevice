644a21a27d23e1676965492d9c8812efdbe57a7d
Tam Tran <tam.half28@gmail.com>
2025-10-29 15:40:12 +0700

    exclude systemmetricautoconfiguration

--- ProductService/src/main/java/intern/lp/ProductServiceApplication.java
+++ ProductService/src/main/java/intern/lp/ProductServiceApplication.java
@@ -1,10 +1,13 @@
 package intern.lp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SystemMetricsAutoConfiguration.class
+})
 @EnableCaching
 public class ProductServiceApplication {
 	public static void main(String[] args) {
