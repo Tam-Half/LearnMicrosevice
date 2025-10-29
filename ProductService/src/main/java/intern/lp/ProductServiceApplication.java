d26a66ed1a776018ba0f737ea5d123d6dc1280ad
Tam Tran <tam.half28@gmail.com>
2025-10-29 15:11:11 +0700

    pull code

--- ProductService/src/main/java/intern/lp/ProductServiceApplication.java
+++ ProductService/src/main/java/intern/lp/ProductServiceApplication.java
@@ -3,10 +3,8 @@ package intern.lp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableCaching
 public class ProductServiceApplication {
 	public static void main(String[] args) {
